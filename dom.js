56bde9506f905911c4b899acb205eae0d9223fb0
Benny Ho <benny.ho2530@gmail.com>
2019-07-01 16:57:18 +0200

    added cards and value for location

--- dom.js
+++ dom.js
@@ -1,22 +1,49 @@
-function performGetRequest1() {
-    var resultElement = document.getElementById('getResult1');
-    resultElement.innerHTML = '';
+/*
+const key = "3c581e97bdd6665208a6ff2c1592317e";
 
-    axios.get('http://jsonplaceholder.typicode.com/todos')
-        .then(function (response) {
-            resultElement.innerHTML = generateSuccessHTMLOutput(response);
-        })
-        .catch(function (error) {
-            resultElement.innerHTML = generateErrorHTMLOutput(error);
-        })
+// You can also use ID
+const button = document.getElementById("button");
+const input = document.getElementById("input-text");
+console.log(button, input);
+
+// Axios is a tool that fetches the API
+/*
+input.addEventListener("keypress", getValueOfInput);
+// Scope and closure: you cant have acces to a var outside of this
+function getValueOfInput(event) {
+  console.log(event.target);
+}
+*/
+
+//Await till you get response of API, then...
+//City value has to be in the function!
+
+const key = "3c581e97bdd6665208a6ff2c1592317e";
+// You can also use ID
+const button = document.getElementsByTagName("button");
+const input = document.getElementById("input-text");
+
+/*Axios is a tool that fetches the API */
+
+input.addEventListener("keypress", getValueOfInput);
+
+/*Scope and closure: you cant have acces to a var outside of this */
+function getValueOfInput(e) {
+  console.log(e.target.value);
 }
 
-function generateSuccessHTMLOutput(response) {
-    return '<h4>Result:</h4>' +
-        '<h5>Status:</h5>' +
-        '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
-        '<h5>Headers:</h5>' +
-        '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
-        '<h5>Data:</h5>' +
-        '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+function getWeather() {
+  button.addEventListener("click", async function() {
+    let city = input.value;
+    let api = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&mode=json&&APPID=${key}`;
+    let response = await axios.get(api);
+    //displayWeather(response);
+    console.log(response.data);
+  });
 }
+
+/*
+function displayWeather(){
+getElementById("description").value
+}
+*/
