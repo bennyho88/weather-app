7a3c3b56cd6420fb9112c2d94903a6e41d38b078
Benny Ho <benny.ho2530@gmail.com>
2019-07-05 11:12:17 +0200

    added icons and celcius

--- dom.js
+++ dom.js
@@ -3,7 +3,7 @@ const apiKey = "3c581e97bdd6665208a6ff2c1592317e";
 const button = document.getElementById("searchBtn");
 const input = document.getElementById("input");
 const dayOutput = document.getElementsByTagName("h3");
-const weatherIcon = document.getElementById("w-icon");
+const weatherIcon = document.getElementById("icon");
 
 let descriptionOne = document.getElementById("w-description-1");
 let descriptionTwo = document.getElementById("w-description-2");
@@ -38,11 +38,11 @@ function getWeather() {
     descriptionFour.innerHTML = response.data.list[24].weather[0].main;
     descriptionFive.innerHTML = response.data.list[32].weather[0].main;
 
-    mainTempOne.innerHTML = response.data.list[0].main.temp;
-    mainTempTwo.innerHTML = response.data.list[8].main.temp;
-    mainTempThree.innerHTML = response.data.list[16].main.temp;
-    mainTempFour.innerHTML = response.data.list[24].main.temp;
-    mainTempFive.innerHTML = response.data.list[32].main.temp;
+    mainTempOne.innerHTML = response.data.list[0].main.temp + "°";
+    mainTempTwo.innerHTML = response.data.list[8].main.temp + "°";
+    mainTempThree.innerHTML = response.data.list[16].main.temp + "°";
+    mainTempFour.innerHTML = response.data.list[24].main.temp + "°";
+    mainTempFive.innerHTML = response.data.list[32].main.temp + "°";
 
     windOne.innerHTML = response.data.list[0].wind.deg;
     windTwo.innerHTML = response.data.list[8].wind.deg;
@@ -51,8 +51,35 @@ function getWeather() {
     windFive.innerHTML = response.data.list[32].wind.deg;
 
     console.log(response.data);
+
+    function getIcon() {
+      let iconPlace1 = document.getElementById("icon1");
+      let iconPlace2 = document.getElementById("icon2");
+      let iconPlace3 = document.getElementById("icon3");
+      let iconPlace4 = document.getElementById("icon4");
+      let iconPlace5 = document.getElementById("icon5");
+
+      console.log(iconPlace1);
+      console.log(iconPlace2);
+
+      let icon1 = response.data.list[0].weather[0].icon;
+      let icon2 = response.data.list[8].weather[0].icon;
+      let icon3 = response.data.list[16].weather[0].icon;
+      let icon4 = response.data.list[24].weather[0].icon;
+      let icon5 = response.data.list[32].weather[0].icon;
+
+      iconPlace1.src = "https://openweathermap.org/img/wn/" + icon1 + "@2x.png";
+      iconPlace2.src = `https://openweathermap.org/img/wn/${icon2}@2x.png`;
+      iconPlace3.src = `https://openweathermap.org/img/wn/${icon3}@2x.png`;
+      iconPlace4.src = `https://openweathermap.org/img/wn/${icon4}@2x.png`;
+      iconPlace5.src = `https://openweathermap.org/img/wn/${icon5}@2x.png`;
+      console.log("icon change!!");
+    }
+    getIcon();
   });
 }
+
+getWeather();
 
 // Displaying 5 consecutive days according to the current date
 
@@ -76,5 +103,3 @@ for (d = 0; d < dayOutput.length; d++, day++) {
   }
   dayOutput[d].innerHTML = days[day];
 }
-
-getWeather();
